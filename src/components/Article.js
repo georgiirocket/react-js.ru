6351955f33178c4802e84b6ae2577230ca5dec20
Georgii Romanishko <georgiiromanishko@tut.by>
2020-06-15 21:03:47 +0300

    add button-functional add bootstrap style and first folder for components ArticleList

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Article(props) {
-    const { article } = props
-    const body = <section>{article.text}</section>
+class Article extends Component {
 
-    return (
-        <div>
-            <h2>{article.title}</h2>
-            {body}
-            <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
-        </div>
-    )
+    state = {
+        isOpen: true
+    }
+
+    render() {
+        const { article } = this.props
+        const body = this.state.isOpen && <section className='card-text'>{article.text}</section>
+
+        return (
+            <div className='card mx-auto' >
+                <div className='card-header'>
+                    <h2>
+                        {article.title}
+                        <button onClick={this.handleClick} className='btn btn-primary btn-lg float-right'>
+                            {this.state.isOpen ? 'close' : 'open'}
+                        </button>
+                    </h2>
+                </div>
+                <div className='card-body'>
+                    {body}
+                    <h6 className='card-subtitle text-muted'>
+                        creation date: {(new Date(article.date)).toDateString()}
+                    </h6>
+                </div>
+            </div>
+        )
+    }
+    handleClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
 }
+
+
 
 export default Article
